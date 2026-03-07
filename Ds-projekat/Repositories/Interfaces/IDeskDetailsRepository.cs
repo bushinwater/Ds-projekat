4e9c149490242453019a3dead081cb984521ce3e
Dusan Davcik <dusandavcik004@gmail.com>
2026-03-07 04:13:46 +0100

    dodat DeskDetailsRepository i intefejs

---
+++ Ds-projekat/Repositories/Interfaces/IDeskDetailsRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Ds_projekat.Repositories.Interfaces
+{
+    internal interface IDeskDetailsRepository
+    {
+        int Add(DeskDetails d);
+        bool Update(DeskDetails d);
+        bool Delete(int resourceId);
+        DeskDetails GetByResourceId(int resourceId);
+    }
+}
